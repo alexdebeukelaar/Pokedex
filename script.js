cdfc4ff64cd49fc64e50ea25d2f53c64c753dc50
Alex de Beukelaar <82021515+alexdebeukelaar@users.noreply.github.com>
2021-05-06 11:56:08 +0200

    made the pokemon an separate object

--- script.js
+++ script.js
@@ -5,4 +5,13 @@ async function getPokemon(){
     const response = await fetch('https://pokeapi.co/api/v2/');
     const data = await response.json();
     console.log(data)
+
+
+const pokemon = {};
+pokemon['image'] = data.sprites.font_default;
+pokemon['id'] = data.id;
+pokemon['name'] = data.name;
+pokemon ['moves'] = data.moves;
+
+console.log(pokemon);
 }
